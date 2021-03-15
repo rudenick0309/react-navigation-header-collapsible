a2f12262db4fbe1ad58b09f9dca7e056ba4b4189
sunju <rudenick0309@gmail.com>
2021-03-15 13:46:59 +0900

    주석 제거

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -85,13 +85,7 @@ const MySearchBar = () => (
 );
 
 // HomeScreen 컴포넌트 안에서, 탭뷰로 선언 될 FirstRoute
-const FirstRoute = (
-  {
-    // onScroll,
-    // containerPaddingTop,
-    // scrollIndicatorInsetTop,
-  },
-) => {
+const FirstRoute = ({}) => {
   const {
     onScroll /* Event handler */,
     onScrollWithListener /* Event handler creator */,
@@ -102,9 +96,6 @@ const FirstRoute = (
     progress /* 0.0 ~ 1.0 */,
     opacity /* 1.0 ~ 0.0 */,
   } = useCollapsibleHeader();
-  // console.log('first router onScroll; ', onScroll);
-  // console.log('first router containerPaddingTop; ', containerPaddingTop);
-  // console.log('first router scrollIndicatorInsetTop;', scrollIndicatorInsetTop);
 
   const Item = ({title}) => (
     <View style={styles.item}>
@@ -116,9 +107,6 @@ const FirstRoute = (
 
   return (
     <View style={{flex: 1, backgroundColor: '#ff4081'}}>
-      {/* <CollapsibleSubHeaderAnimator translateY={translateY}>
-        <MySearchBar />
-      </CollapsibleSubHeaderAnimator> */}
       <Animated.FlatList
         onScroll={onScroll}
         contentContainerStyle={{paddingTop: containerPaddingTop}}
@@ -138,16 +126,6 @@ const SecondRoute = () => (
 
 // FirestRoute 와 SecondRoute 를 탭뷰로 감싸 줄 컴포넌트
 function HomeScreen() {
-  // const {
-  //   onScroll /* Event handler */,
-  //   onScrollWithListener /* Event handler creator */,
-  //   containerPaddingTop /* number */,
-  //   scrollIndicatorInsetTop /* number */,
-  //   /* Animated.AnimatedInterpolation by scrolling */
-  //   translateY /* 0.0 ~ -headerHeight */,
-  //   progress /* 0.0 ~ 1.0 */,
-  //   opacity /* 1.0 ~ 0.0 */,
-  // } = useCollapsibleSubHeader();
   const layout = useWindowDimensions();
 
   const [index, setIndex] = React.useState(0);
@@ -156,23 +134,19 @@ function HomeScreen() {
     {key: 'second', title: 'Second'},
   ]);
 
-  const renderTabBar = (props) => (
-    <TabBar
-      {...props}
-      indicatorStyle={{backgroundColor: 'white', height: 100}}
-      style={{backgroundColor: 'yellow', height: 100, paddingTop: 116}}
-    />
-  );
+  // const renderTabBar = (props) => (
+  //   <TabBar
+  //     {...props}
+  //     indicatorStyle={{backgroundColor: 'white', height: 100}}
+  //     style={{backgroundColor: 'yellow', height: 100, paddingTop: 116}}
+  //   />
+  // );
+
+  // 각 컴포넌트에 프랍스를 전달하기 위한 renderScene
   const renderScene = ({route}) => {
     switch (route.key) {
       case 'first':
-        return (
-          <FirstRoute
-          // onScroll={onScroll}
-          // containerPaddingTop={containerPaddingTop}
-          // scrollIndicatorInsetTop={scrollIndicatorInsetTop}
-          />
-        );
+        return <FirstRoute />;
       case 'second':
         return <SecondRoute />;
       default:
@@ -182,20 +156,7 @@ function HomeScreen() {
 
   return (
     <>
-      {/* <CollapsibleSubHeaderAnimator translateY={translateY}>
-        <View
-          style={{
-            padding: 15,
-            width: '100%',
-            backgroundColor: 'blue',
-            height: 60,
-            zIndex: 999,
-          }}>
-          <TextInput placeholder="search here" />
-        </View>
-      </CollapsibleSubHeaderAnimator> */}
       <TabView
-        // renderTabBar={renderTabBar}
         navigationState={{index, routes}}
         renderScene={renderScene}
         onIndexChange={setIndex}
@@ -220,7 +181,5 @@ const styles = StyleSheet.create({
     height: 100,
     opacity: 0.2,
   },
-  title: {
-    // fontSize: 32,
-  },
+  title: {},
 });
